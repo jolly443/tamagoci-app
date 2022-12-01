8b35b3dbb3a1d1c0f5cdf4ba15337f4a09f56e31
Malte Bendl <malte.bendl.ext@siemens-healthineers.com>
2022-12-01 16:13:55 +0100

    WIP: Change egg to husky

--- src/Tamagotchi.js
+++ src/Tamagotchi.js
@@ -1,33 +1,55 @@
+import { 
+  useEffect,
+  useState
+ } from 'react'
 import husky_default from './assets/husky-default.png';
 import egg_closed from './assets/egg-closed.png'
 import './styles/Tamagotchi.css';
 
+
 function Tamagotchi() {
+  // Timer
+  const [counter, setCounter] = useState(60);
+  useEffect(() => {
+    counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+  }, [counter]);
+
+  // Pet picture
+  const [petPic, setPetPic] = useState(egg_closed);
+  useEffect(() => {
+    if(counter < 58) {
+      setPetPic(husky_default)
+    }
+  })
+  
+
+
+
   const petName = "Ei";
   var petStatus = "noch nicht geschlüpft";
   var timeToHatch = "7 Stunden, 0 Minuten, 0 Sekunden";
 
   return (
-    <body className="App">
+    <div className="App">
       <header>
-        <h1 class="pet-name">{petName}</h1>
-        <img src={egg_closed} alt="Pet" className="pet pet-border"></img>
-        <p class="pet-status">
+        <h1 className="pet-name">{petName}</h1>
+        <img src={petPic} alt="Pet" className="pet pet-border"></img>
+        <p className="pet-status">
           Dein Tamagotchi ist {petStatus}
         </p>
         <p>
-          Tage übrig bis zum Schlüpfen: {timeToHatch}
+          Tage übrig bis zum Schlüpfen: {counter}
         </p>
-        <div class="container">
-          <button class="button">Lichtschalter</button>
-          <button class="button">Temperatur erhöhen</button>
-          <button class="button">Temperatur senken</button>
-          <button class="button">Füttern</button>
-          <button class="button">Sauber machen</button>
-          <button class="button">Spielen</button>
+        <div className="container">
+          <button className="button">Lichtschalter</button>
+          <button className="button">Temperatur erhöhen</button>
+          <button className="button">Temperatur senken</button>
+          <button className="button">Füttern</button>
+          <button className="button">Sauber machen</button>
+          <button className="button">Spielen</button>
         </div>
       </header>
-    </body>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import reportWebVitals from './reportWebVitals';
-import StartScreen from './StartScreen';
+import Tamagotchi from './Tamagotchi';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<StartScreen />);
+root.render(
+    <Tamagotchi />
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
