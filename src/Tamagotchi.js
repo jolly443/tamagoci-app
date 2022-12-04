e64a5c9038829bfa287d2282490d50f3bdb7e4ea
Malte Bendl <malte.bendl.ext@siemens-healthineers.com>
2022-12-04 16:10:51 +0100

    CSS & timing dependencies

--- src/Tamagotchi.js
+++ src/Tamagotchi.js
@@ -21,30 +21,33 @@ class Tamagotchi extends Component {
       petPic: egg_closed,
       isHatched: false
     }
-    this.updateHatchTimer = this.updateHatchTimer.bind(this) 
+    this.updateHatchTimer = this.updateHatchTimer.bind(this)
   }
 
   updateHatchTimer(val) {
-    this.setState({hatchTimer: val});
-    if(this.state.hatchTimer % 2 == 0) {
+    this.setState({ hatchTimer: val });
+    if (this.state.hatchTimer % 2 == 0) {
       // Half of timer passed
-      this.setState({petPic: egg_cracked})
+      this.setState({ petPic: egg_cracked })
+      this.setState({ petStatus: "dabei zu schlüpfen" })
     }
-    if(this.state.hatchTimer == 0) {
+    if (this.state.hatchTimer == 0) {
       // Timer passed, egg hatches
-      this.setState({isHatched: true})
-      this.setState({petPic: husky_default})
+      this.setState({ isHatched: true })
+      this.setState({ petPic: husky_default })
+      this.setState({ petStatus: "geschlüpft" })
+      const petName = document.getElementById('petName');
+      const hatchTimer = document.getElementById('hatchTimer');
+      hatchTimer?.classList.add("hidden")
+      hatchTimer?.classList.remove("shown")
+      petName?.classList.add("shown")
+      petName?.classList.remove("hidden")
     }
   }
 
-
-  // // Set pet (egg) picture depending on timer
-  // const [petName, setPetName] = useState('Ei');
-  // const [petStatus, setPetStatus] = useState('noch nicht geschlüpft');
-  // const [petPic, setPetPic] = useState(egg_closed);
-  // handleSubmit = (event) => {
-  //   event.preventDefault();
-  // }
+  handleSubmit = (event) => {
+    event.preventDefault();
+  }
   // useEffect(() => {
   //   if (hatchTimer < 5) {
   //     setPetPic(egg_cracked)
@@ -68,12 +71,12 @@ class Tamagotchi extends Component {
     return (
       <div className="App">
         <header>
-          <h1 className="pet-name">{this.state.petName}</h1>
+          <h1 className="hidden pet-name">{this.state.petName}</h1>
           <img src={this.state.petPic} alt="Pet" className="pet pet-border"></img>
           <p id="petStatus" className="pet-status">
             Dein Tamagotchi ist {this.state.petStatus}
           </p>
-          <div className="container">
+          <div id="petName" className="container hidden">
             <form onSubmit={null}>
               <label>Du kann deinem Tamagotchi nun einen Namen geben:
                 <input
@@ -85,7 +88,9 @@ class Tamagotchi extends Component {
               <input type="submit" />
             </form>
           </div>
-          <HatchTimer countdownInDays={1} updateHatchTimer={this.updateHatchTimer}/>
+          <div id="hatchTimer">
+            <HatchTimer countdownInDays={1} updateHatchTimer={this.updateHatchTimer} />
+          </div>
           <div className="container">
             <button className="button">Lichtschalter</button>
             <button className="button">Temperatur erhöhen</button>
